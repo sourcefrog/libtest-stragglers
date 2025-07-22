f227c68157b46df551d5315794cdf4b56c032cec
Martin Pool <mbp@sourcefrog.net>
2025-07-21 19:31:42 -0700

    Demonstrate nextest behavior with stragglers

---
+++ tests/main.rs
@@ -0,0 +1,22 @@
+use std::{thread::sleep, time::Duration};
+
+#[test]
+fn fast_passes() {
+    assert!(true);
+}
+
+#[test]
+fn fast_passes_2() {
+    assert!(true);
+}
+
+#[test]
+fn fast_fails() {
+    assert!(false, "This test should fail");
+}
+
+#[test]
+fn slow_passes() {
+    sleep(Duration::from_secs(10));
+    assert!(true);
+}
